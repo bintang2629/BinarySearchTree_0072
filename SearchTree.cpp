a7c92b9d9b0520114e0d92a8a9325981be051a27
bintang2629 <bimbimpratama23@gmail.com>
2026-06-11 18:17:56 +0700

    bagian case 1

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -141,7 +141,18 @@ int main ()
         cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5): ";
 
-        
+        char ch;
+        cin >> ch;
+
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout <<
+        }
+        }
     }
 
 }
