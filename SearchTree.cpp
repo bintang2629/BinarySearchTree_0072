feb14f9940c7705561dc7c658e9d20dd16f516c0
bintang2629 <bimbimpratama23@gmail.com>
2026-06-11 17:58:59 +0700

    BAGIANthis function searches the node of the specified node

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -40,5 +40,29 @@ public:
 
         Node *parent = NULL;
         Node *currentNode = ROOT;
+
+        search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT = newNode;
+            return;
+        }
+
+        if (element < parent->info)
+        {
+          parent ->leftchild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent ->rightchild = newNode;
+        }
+    }
+
+    //this function searches the node of the specified node
+    //as well as the current node of its parrent
+    void search(string element, Node *&parent,Node *&currentNode)
+    {
+       currentNode = ROOT;
     }
 }
